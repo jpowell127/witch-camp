6ac29e30c7f473f5cba8bb996d24be3aa74a63b4
Jonathan Powell <2856365+jpowell127@users.noreply.github.com>
2024-03-24 12:54:29 -0500

    feat: add landing page and navigation

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'tailwindui.com',
+        port: '',
+        pathname: '/img/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
