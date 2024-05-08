5fadc7671a11e60c9dbbf9e2ac97dc0e4620e031
Jonathan Powell <2856365+jpowell127@users.noreply.github.com>
2024-05-08 13:35:01 -0500

    feat: add about page dynamic query

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,17 +1,30 @@
 import { client } from '@/utils/client';
 
-const About = () => {
+type AboutPage = {
+  title: string;
+  text: string;
+};
+
+export default async function About() {
+  const [{ title, text }] = await client.fetch<AboutPage[]>(
+    `*[_type == "page" && title == "About Witch Camp"] {
+    title,
+    "text": content[0].children[0].text
+  }`,
+    {},
+    {
+      next: {
+        revalidate: 60,
+      },
+    },
+  );
+
   return (
     <div className='text-center'>
       <h1 className='text-4xl font-bold tracking-tight text-white sm:text-6xl'>
-        About Witch Camp
+        {title}
       </h1>
-      <p className='mt-6 text-lg leading-8 text-gray-300'>
-        Join us under the moonlit skies for nightly rituals that celebrate the
-        cycles of the earth and the magic within us all. Connect with a
-        community of like-minded souls as we weave spells, share stories, and
-        revel in the mystical energies that bind us.
-      </p>
+      <p className='mt-6 text-lg leading-8 text-gray-300'>{text}</p>
       <div className='mt-10 flex items-center justify-center gap-x-6'>
         <a href='#' className='text-sm font-semibold leading-6 text-white'>
           Learn more <span aria-hidden='true'>→</span>
@@ -19,6 +32,4 @@ const About = () => {
       </div>
     </div>
   );
-};
-
-export default About;
+}
--- utils/client.ts
+++ utils/client.ts
@@ -4,6 +4,10 @@ const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
 const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2023-05-03';
 
+console.log('<---------o0o--------->');
+console.log('projectId:', projectId);
+console.log('<---------o0o--------->');
+
 export const client = createClient({
   projectId,
   dataset,
